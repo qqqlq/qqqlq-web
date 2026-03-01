c1b12e1c69a0cf2ee3fa3d561e097f59f3de7097
Yasu <ychihiro0315@icloud.com>
2026-03-01 15:26:51 +0900

    サイトのファビコンを自分のアイコン画像に変更

--- src/components/ui/layout/NavBar.tsx
+++ src/components/ui/layout/NavBar.tsx
@@ -1,4 +1,4 @@
-import { 
+import {
     Box,
     Flex,
     Stack,
@@ -14,29 +14,29 @@ import { useNavigate } from "react-router-dom";
 
 // ナビゲーションバー本体
 const NavBar = () => {
-  
-  const navigate = useNavigate();
 
-  const goToTop = () => {
-    navigate("/")
-  };
+    const navigate = useNavigate();
 
-  return (
-    <>
-        <Box bg={useColorModeValue('gray.200', 'gray.900')} px={4}>
-            <Flex h={16} alignItems="center" justifyContent="space-between">
-                <Box className={classes.NavBarTitle} onClick={goToTop} cursor="pointer" >Node Walker</Box>
-                <Stack direction="row" alignItems="center">
-                    <ColorModeButton />
-                    <Avatar.Root onClick={() => window.open('https://github.com/chihiroyasu')}>
-                        <Avatar.Fallback name="QQQLQ" />
-                        <AvatarImage src="/qqqlq_icon.jpg"/>
-                    </Avatar.Root>
-                </Stack>
-            </Flex>
-        </Box>
-    </>
-  )
+    const goToTop = () => {
+        navigate("/")
+    };
+
+    return (
+        <>
+            <Box bg={useColorModeValue('gray.200', 'gray.900')} px={4}>
+                <Flex h={16} alignItems="center" justifyContent="space-between">
+                    <Box className={classes.NavBarTitle} onClick={goToTop} cursor="pointer" >Node Walker</Box>
+                    <Stack direction="row" alignItems="center">
+                        <ColorModeButton />
+                        <Avatar.Root onClick={() => window.open('https://github.com/qqqlq')}>
+                            <Avatar.Fallback name="QQQLQ" />
+                            <AvatarImage src="/qqqlq_icon.jpg" />
+                        </Avatar.Root>
+                    </Stack>
+                </Flex>
+            </Box>
+        </>
+    )
 }
 
 export default NavBar
